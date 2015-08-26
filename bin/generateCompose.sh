47cd34e8b406cf9852ecc9ac2e093eb12c4514f5
hoeghh <heh@praqma.net>
2015-08-26 09:56:45 +0200

    It is now possible to choose what app to start in staci.properties. Updated README.md

--- bin/generateCompose.sh
+++ bin/generateCompose.sh
@@ -4,6 +4,21 @@ source $STACI_HOME/functions/tools.f
 # Set version of images
 version=$(getProperty "imageVersion")
 
+# Find out what to start
+start_mysql=$(getProperty "start_mysql")
+start_jira=$(getProperty "start_jira")
+start_confluence=$(getProperty "start_confluence")
+start_bamboo=$(getProperty "start_bamboo")
+
+if [ "$start_mysql" == "1" ]; then
+   dblink="links:
+    - atlassiandb
+"
+else
+   dblink=""
+fi
+
+if [ "$start_jira" == "1" ]; then
 cat << EOF
 jira:
   image: staci/jira:$version
@@ -11,25 +26,36 @@ jira:
     - "8080"
   ports:
     - "8080:8080"
-  links:
-    - atlassiandb
+  $dblink
+EOF
+
+fi
+if [ "$start_confluence" == "1" ]; then
+cat << EOF
 confluence:
   image: staci/confluence:$version
   expose:
     - "8090"
   ports:
     - "8090:8090"
-  links:
-    - atlassiandb
+  $dblink
+EOF
+fi
+
+if [ "$start_bamboo" == "1" ]; then
+cat << EOF
 bamboo:
   image: staci/bamboo:$version
   expose:
     - "8085"
   ports:
     - "8085:8085"
-  links:
-    - atlassiandb
+  $dblink
+EOF
+fi
 
+if [ "$start_mysql" == "1" ]; then
+cat << EOF
 atlassiandb:
   image: staci/atlassiandb:$version
   expose:
@@ -41,3 +67,4 @@ atlassiandb:
     - MYSQL_PASSWORD="nlpw"
     - MYSQL_USER="jira"
 EOF
+fi
