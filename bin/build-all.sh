799b5e64166da70658b671a1fce879a7c5006102
hoeghh <heh@praqma.net>
2015-09-17 13:49:19 +0200

    Fix bug in build. Now only building specified images

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -4,43 +4,53 @@ source $STACI_HOME/functions/tools.f
 # Set version of images
 version=$(getProperty "imageVersion")
 
-# Here we will find all containers
-images=$(ls $STACI_HOME/images/)
+# Find out what to start
+start_mysql=$(getProperty "start_mysql")
+start_jira=$(getProperty "start_jira")
+start_confluence=$(getProperty "start_confluence")
+start_bamboo=$(getProperty "start_bamboo")
 
 # Get context path for each application
 jiraContextPath=$(getProperty "jira_contextpath")
 confluenceContextPath=$(getProperty "confluence_contextpath")
 bambooContextPath=$(getProperty "bamboo_contextpath")
 
-# Set context path for Jira
-if [ ! -z "$jiraContextPath" ]; then
+# Set context path and build Jira
+if [ ! -z "$jiraContextPath" ] && [ "$start_jira" == "1" ]; then
   jiraContextPath='\'$jiraContextPath
   echo "sed -i -e 's/<Context path=\"\"/<Context path=\"$jiraContextPath\"/g' /opt/atlassian/jira/conf/server.xml" > $STACI_HOME/images/jira/context/setContextPath.sh
   chmod u+x $STACI_HOME/images/jira/context/setContextPath.sh
+  docker build -t staci/jira:$version $STACI_HOME/images/jira/context/ > $STACI_HOME/logs/jira.build.log 2>&1 &
+
+  # clean up
+  rm $STACI_HOME/images/jira/context/setContextPath.sh
 fi
 
-# Set context path for Confluence
-if [ ! -z "$confluenceContextPath" ]; then
+# Set context path and build Confluence
+if [ ! -z "$confluenceContextPath" ] && [ "$start_confluence" == "1" ]; then
   confluenceContextPath='\'$confluenceContextPath
   echo "sed -i -e 's/<Context path=\"\"/<Context path=\"$confluenceContextPath\"/g' /opt/atlassian/confluence/conf/server.xml" > $STACI_HOME/images/confluence/context/setContextPath.sh
   chmod u+x $STACI_HOME/images/confluence/context/setContextPath.sh
+  docker build -t staci/confluence:$version $STACI_HOME/images/confluence/context/ > $STACI_HOME/logs/confluence.build.log 2>&1 &
+
+  # clean up
+  rm $STACI_HOME/images/confluence/context/setContextPath.sh
 fi
 
-# Set context path for Bamboo
-if [ ! -z "$bambooContextPath" ]; then
+# Set context path and build Bamboo
+if [ ! -z "$bambooContextPath" ] && [ "$start_bamboo" == "1" ]; then
   bambooContextPath='\'$bambooContextPath
   echo "sed -i -e 's/<Context path=\"\"/<Context path=\"$bambooContextPath\"/g' /opt/atlassian/bamboo/conf/server.xml" > $STACI_HOME/images/bamboo/context/setContextPath.sh
   chmod u+x $STACI_HOME/images/bamboo/context/setContextPath.sh
+  docker build -t staci/bamboo:$version $STACI_HOME/images/bamboo/context/ > $STACI_HOME/logs/bamboo.build.log 2>&1 &
+
+  # clean up
+  rm $STACI_HOME/images/bamboo/context/setContextPath.sh
 fi
 
-# Lets build all the found images
-for image in $images; do
-  echo " - Building : $image -- please wait..."
-  docker build -t staci/$image:$version $STACI_HOME/images/$image/context/ > $STACI_HOME/logs/"$image".log 2>&1 &
-done
-wait
+# Build mysql database as atlassiandb
+if [ "$start_mysql" == "1" ]; then
+  docker build -t staci/atlassiandb:$version $STACI_HOME/images/atlassiandb/context/ > $STACI_HOME/logs/atlassiandb.build.log 2>&1 &
+fi
 
-# Cleanup
-rm $STACI_HOME/images/jira/context/setContextPath.sh
-rm $STACI_HOME/images/confluence/context/setContextPath.sh
-rm $STACI_HOME/images/bamboo/context/setContextPath.sh
+wait
