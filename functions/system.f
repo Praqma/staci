dfb8257a1c290fc4587c91f571732aa19e4e5d30
hoeghh <heh@praqma.net>
2016-05-27 15:43:19 +0200

    fix #44 - check docker dependencies

---
+++ functions/system.f
@@ -0,0 +1,57 @@
+check_docker_dependencies() {
+  local check_result=0
+  supported_docker_version="1.11.1"
+  supported_docker_compose_version="1.6.0"
+  supported_docker_machine_version="0.6.0"
+
+  # Check that docker is installed, and has the right version
+  #
+  echo " - Running dependency checks, please wait..."
+  if command_exists "docker";  then
+     docker_version=$(docker version --format '{{.Client.Version}}')
+     echo -ne "  - Docker version\t\t: $docker_version"
+     if $(do_version_check $docker_version $supported_docker_version);then
+       echo -e " - OK"
+     else
+       echo -e " - Error, please upgrade to $supported_docker_version"
+       return 1
+     fi
+  else
+     echo "  - Docker not installed, please install at lest version $supported_docker_version"
+     return 1
+  fi
+
+  # Check that docker-compose is installed, and has the right version
+  #
+  if command_exists docker-compose; then
+     docker_compose_version=$(docker-compose version --short)
+     echo -ne "  - Docker-compose version\t: $docker_compose_version"
+     if $(do_version_check $docker_compose_version $supported_docker_compose_version); then
+       echo -e "\t - OK"
+     else
+       echo -e "\t - Error, please upgrade to $supported_docker_compose_version" 
+       return 1
+     fi
+  else
+     echo "  - Docker-compose not installed, please install at lest version $supported_docker_compose_version"
+     return 1
+  fi
+
+  # Check that docker-machine is installed, and has the right version
+  # 
+  if command_exists docker-machine; then
+     docker_machine_version=$(docker-machine version | cut -d"," -f1|cut -d" " -f3)
+     echo -ne "  - Docker-machine version\t: $docker_machine_version"
+     if $(do_version_check $docker_machine_version $supported_docker_machine_version); then
+       echo -e "\t - OK"
+     else
+       echo -e "\t - Error, please upgrade to $supported_docker_machine_version" 
+       return 1
+     fi
+  else
+     echo "  - Docker-machine not installed, please install at lest version $supported_docker_machine_version"
+     return 1
+  fi
+  return 0
+}
+
--- functions/tools.f
+++ functions/tools.f
@@ -36,3 +36,43 @@ function getContainerIP(){
     containerName=$1
     docker inspect --format '{{ .NetworkSettings.IPAddress }}' $containerName
 }
+
+do_version_check() {
+# http://stackoverflow.com/questions/4023830/bash-how-compare-two-strings-in-version-format
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 0
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+#
+# command_exists checks if a host program exists or not
+#
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
