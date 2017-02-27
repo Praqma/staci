8bdafc457a66e40e5ca1286e9a6fe635c8109f17
kamran <kamran@dockerhost.example.com>
2017-02-27 11:44:04 +0100

    Made some improvements in the simple_ci ... WIP

--- setup.sh
+++ setup.sh
@@ -1,85 +1,134 @@
 #!/bin/bash
 
-CI_HOME=$(pwd)
+# Note: this script needs to run as root or as sudo.
+
+ME=$(whoami)
+
+if [ "${ME}" != "root" ] ; then
+  echo "This script needs to run as root or using sudo. Exiting ..."
+  exit 1
+fi
+
+########### START - USER DEFINED VARIABLES - ###############################
+#
+#
+
+SETUP_DIR=$(pwd)
+SETUP_DIR=${SETUP_DIR}
+CONFIG_FILE=setup.conf
+
+#
+#
+########### END - USER DEFINED VARIABLES - ################################
+
+# Load some config variables from the setup.conf file.
+if [ ! -r ${CONFIG_FILE} ] ; then
+  echo "${CONFIG_FILE} is not found. Need to know the URLs for various software. Exiting ... "
+  exit 1
+else
+  echo "Loading values for JIRA_URL , CRUCIBLE_URL, BITBUCKET_URL and MYSQL_CONNECTOR_URL ..."
+  source {CONFIG_FILE}
+fi
+
+########### START - SYTEM DEFINED VARIABLES - #############################
+#
+#
+# The command basename only returns the last (filename) portion of any long path / URL.  
+
+JIRA_TARBALL=$(basename $JIRA_URL)
+CRUCIBLE_TARBALL=$(basename $CRUCIBLE_URL)
+BITBUCKET_TARBALL=$(basename $BITBUCKET_URL)
+MYSQL_CONNECTOR_TARBALL=$(basename $MYSQL_CONNECTOR_URL)
+
+# Define the UID and GID for the directories and files ...
+USER_ID=1000
+GROUP_ID=1000
+
+#
+#
+########### END - SYTEM DEFINED VARIABLES - ##############################
+
+
+########### START - Main Program - #######################################
+#
+#
+
 source haproxy_ssl_setup.f
 
-# Set ownership of directories and files
-user_id=1000
-group_id=1000
 
-#Download tar balls for Atlassian products
+echo "Downloading Atlassian software products ..."
+echo "- Jira ..."
+curl -s -# -z -o images/jira/${JIRA_TARBALL} ${JIRA_URL}
 
-if [ ! -f images/jira/atlassian-jira-software-7.3.1.tar.gz ]; then
-	echo "####### Downloading jira ########"
-	echo
-	wget -q "https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.3.1.tar.gz" -O images/jira/atlassian-jira-software-7.3.1.tar.gz
-fi
-echo -e "Skipping Jira download\; tarball is available locally\n"
+echo "- Crucible ..."
+curl -s -# -z -o images/jira/${CRUCIBLE_TARBALL} ${CRUCIBLE_URL}
 
-if [ ! -f images/jira/mysql-connector-java-5.1.36.tar.gz ]; then 
-	 echo "####### Downloading mysql connector  ########"
-	wget -q "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.36.tar.gz" -O images/jira/mysql-connector-java-5.1.36.tar.gz
-fi
-echo -e "Skipping mysql connector  download\; tarball is available in jira image dir\n"
+echo "- Bit Bicket ..."
+curl -s -# -z -o images/jira/${BITBUCKET_TARBALL} ${BITBUCKET_URL}
 
-if [ ! -f images/crucible/crucible-4.2.0.zip ]; then
-	echo "####### Downloading Crucible ########"
-	echo
-	wget -q "https://www.atlassian.com/software/crucible/downloads/binary/crucible-4.2.0.zip" -O images/crucible/crucible-4.2.0.zip
-fi
-echo -e "Skipping crucible  download\; tarball is available locally\n"
+echo "- MYQL Connector ..."
+curl -s -# -z -o images/jira/${MYSQL_CONNECTOR_TARBALL} ${MYSQL_CONNECTOR_URL}
 
-if [ ! -f images/crucible/mysql-connector-java-5.1.36.tar.gz ]; then
-	cp images/jira/mysql-connector-java-5.1.36.tar.gz images/crucible/mysql-connector-java-5.1.36.tar.gz
-fi
-if [ ! -f images/bitbucket/atlassian-bitbucket-4.10.1.tar.gz ]; then
-	wget -q "https://www.atlassian.com/software/stash/downloads/binary/atlassian-bitbucket-4.10.1.tar.gz" -O images/bitbucket/atlassian-bitbucket-4.10.1.tar.gz
-fi
-if [ ! -f images/bitbucket/mysql-connector-java-5.1.36.tar.gz ]; then
-        cp images/jira/mysql-connector-java-5.1.36.tar.gz images/bitbucket/mysql-connector-java-5.1.36.tar.gz
+# Notice that MySQL connector is downloaded directly in Jira's image location.
+
+
+echo "Copying MySQL connector tarball inside each atlassian product ..."
+# Copy from Jira to other two atlassian products.
+
+if [ ! -r images/crucible/${MYSQL_CONNECTOR_TARBALL} ]; then
+	cp images/jira/${MYSQL_CONNECTOR_TARBALL}  images/crucible/${MYSQL_CONNECTOR_TARBALL}
 fi
 
-#Create volume directories for persistent data storage
-if [ !  -d "$CI_HOME/logs" ]; then
-        sudo mkdir $CI_HOME/logs
-	chown -R 1000:1000 $CI_HOME/logs
+if [ ! -r images/bitbucket/${MYSQL_CONNECTOR_TARBALL} ]; then
+        cp images/jira/${MYSQL_CONNECTOR_TARBALL}  images/bitbucket/${MYSQL_CONNECTOR_TARBALL}
 fi
 
-if [ !  -d "/opt/simple_ci/" ]; then
-	sudo mkdir /opt/simple_ci
+echo "Creating logs directory in the root of setup directory - ${SETUP_DIR} ..."
+
+if [ !  -d $SETUP_DIR/logs ]; then
+        mkdir $SETUP_DIR/logs
+	chown -R 1000:1000 $SETUP_DIR/logs
+fi
+
+
+echo "Creating volume directories for persistent data storage in:  ${STORAGE_DIR}..."
+if [ !  -d ${STORAGE_DIR} ]; then
+	mkdir -p ${STORAGE_DIR}
 fi
 
 for service in haproxy jira crucible bitbucket artifactory jenkins; do
-	if [  ! -d "/opt/simple_ci/$service" ]; then
-		sudo mkdir /opt/simple_ci/$service
+	if [  ! -d "${STORAGE_DIR}/$service" ]; then
+		mkdir ${STORAGE_DIR}/$service
 		if [ $service == "artifactory" ]; then
-			sudo mkdir /opt/simple_ci/$service/backup /opt/simple_ci/$service/data /opt/simple_ci/$service/logs
+			mkdir -p ${STORAGE_DIR}/$service/backup ${STORAGE_DIR}/$service/data ${STORAGE_DIR}/$service/logs
 			
 		fi
 	fi
 done
-chown -R 1000:1000 /opt/simple_ci
 
-# Generate SSL certificates for haproxy
+chown -R 1000:1000 ${STORAGE_DIR}
+
+echo "Generating self-signed SSL certificates for haproxy"
 
 setupHaproxySSLcrt  > /dev/null 2>&1
 
-# build java base image for jira, crucible, bitbucket
+# build java base image for jira, crucible and bitbucket
 image_check=$(docker images | grep praqma/java)
+
 if [ -z "$image_check" ]; then
-	echo "###### building the base image ########"
+	echo "###### Building the base image: praqma/java_8 ########"
 	echo
-	docker build -t "praqma/java_8"  $CI_HOME/images/base/. >> $CI_HOME/logs/base.log 2>&1  
+	docker build -t "praqma/java_8"  $SETUP_DIR/images/base/. >> $SETUP_DIR/logs/base.log 2>&1  
 fi
 
 
 wait
 
-echo "####### Spinning up the stack database ########"
+echo "####### Spinning up  database for initial configuration ########"
 echo
 
 
-docker-compose up -d atlassiandb > $CI_HOME/logs/docker-compose.atlassiandb.log 2>&1
+docker-compose up -d atlassiandb > $SETUP_DIR/logs/docker-compose.atlassiandb.log 2>&1
 
   
  # Wait for MySql to start up
@@ -126,7 +175,7 @@ sleep 1
 echo
 echo "######## spinning up the rest of the stack ########"
 echo
-docker-compose up -d >> $CI_HOME/logs/docker-compose.log 2>&1
+docker-compose up -d >> $SETUP_DIR/logs/docker-compose.log 2>&1
 
 echo " >>>>>>>>>>>>>>>> Installation Complete! <<<<<<<<<<<<<<<<<"
 
