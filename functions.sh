cb33f830f9be51c683b35844da1d64027e1de0fb
Kamran Azeem <kaz@praqma.net>
2017-02-28 15:53:53 +0100

    This is a rename of the haproxy_ssl_setup file

--- functions.sh
+++ functions.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-function setupHaproxySSLcrt(){
+
+function generateSSLCertificate(){
     # Certificate specific variables:
     COUNTRY="NO"
     STATE="Oslo"
@@ -11,14 +12,21 @@ function setupHaproxySSLcrt(){
     # COMMON_NAME="server1.example.com"
     # COMMON_NAME="jenkins.example.com"
     # COMMON_NAME="example.com"
+    # COMMON_NAME="*.example.com"
 
-    COMMON_NAME="*.example.com"
+    COMMON_NAME="*.${DOMAIN_NAME}"
 
     # http://crohr.me/journal/2014/generate-self-signed-ssl-certificate-without-prompt-noninteractive-mode.html
+
     openssl genrsa -des3 -passout pass:x -out /tmp/server.pass.key 2048
+
     openssl rsa -passin pass:x -in /tmp/server.pass.key -out /tmp/server.key
+
     rm /tmp/server.pass.key 
-    openssl req -new -key /tmp/server.key -out /tmp/server.csr -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCATION}/O=${ORGANIZATION}/OU=${ORG_UNIT}/CN=${COMMON_NAME}"
+
+    openssl req -new -key /tmp/server.key -out /tmp/server.csr \
+        -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCATION}/O=${ORGANIZATION}/OU=${ORG_UNIT}/CN=${COMMON_NAME}"
+
     openssl x509 -req -days 365 -in /tmp/server.csr -signkey /tmp/server.key -out /tmp/server.crt
 
     # Combine /tmp/server.key and /tmp/server.crt into a single server.pem file. 
